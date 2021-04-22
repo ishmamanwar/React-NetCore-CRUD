ede5d8f60a8c9fa9052c442a84de5538bb1daf9b
ishmam.anwar96 <ishmam.anwar96@hotmail.com>
2021-04-22 13:04:28 -0600

    Product and Category models created

---
+++ WebAPI/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public bool Active { get; set; } = true;
+
+        public string CategoryDescription { get; set; }
+
+    }
+}
